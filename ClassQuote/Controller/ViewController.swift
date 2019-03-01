789835421fd8a27418dc2631f0d553271f525bc8
hoalex <svt75@hotmail.com>
2019-03-01 15:01:46 +0100

    Interface has been created.

---
+++ ClassQuote/Controller/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  ClassQuote
+//
+//  Created by Alex on 01/03/2019.
+//  Copyright © 2019 Alexandre Holet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var newQuoteButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addShadowToQuoteLabel()
+    }
+
+    private func addShadowToQuoteLabel() {
+        quoteLabel.layer.shadowColor = UIColor.black.cgColor
+        quoteLabel.layer.shadowOpacity = 0.9
+        quoteLabel.layer.shadowOffset = CGSize(width: 1, height: 1)
+    }
+    
+    @IBAction func tappedNewQuoteButton() {
+    }
+    
+}
+
